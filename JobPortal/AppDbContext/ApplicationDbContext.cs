69a6bd4d3fe287e9ed7d85aee244275c08718a5e
Hammad Hassan <hammad.hassan@purelogics.com>
2023-03-28 17:22:16 +0500

    Add project files.

---
+++ JobPortal/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JobPortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortal.AppDbContext
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
---
+++ JobPortal/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace JobPortal.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public string JobStatus { get; set; }
+
+
+    }
+}
