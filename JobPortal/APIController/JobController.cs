73a1ef406608e527232e6d71c0325f5499826e32
Hammad Hassan <hammad.hassan@purelogics.com>
2023-05-16 18:32:16 +0500

    new changes

--- JobPortal/APIController/JobController.cs
+++ JobPortal/APIController/JobController.cs
@@ -1,8 +1,11 @@
-﻿using JobPortal.Models;
+﻿using JobPortal.Enums;
+using JobPortal.Models;
+using JobPortal.Models.Dto;
 using JobPortal.Services.IRepository;
 using JobPortal.Services.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 using System.Net.Http.Headers;
 
 namespace JobPortal.APIController
@@ -84,15 +87,89 @@ namespace JobPortal.APIController
         }
         // POST api/Job
         [HttpPost("create")]
-        public async Task<Job> Create(Job job)
+        public async Task<IActionResult> Create( Job job)
         {
-           
+
+            /*if (job.Icon == null || job.Icon.Length == 0)
+                return BadRequest("No image uploaded.");
+
+            // Generate a unique image file name
+            var imageFileName = Guid.NewGuid().ToString() + Path.GetExtension(job.Icon.FileName);
+
+            // Save the image to a folder on the server
+            var imagePath = Path.Combine("C:\\Users\\Hammad\\source\\repos\\JobPortal\\JobPortal\\wwwroot\\uploadedFiles\\", imageFileName);
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                await job.Icon.CopyToAsync(stream);
+            }
+
+            // Create a new product entity
+            var createJob = new Job
+            {
+                Icon = imageFileName,
+                Title = job.Title,
+                Description = job.Description,
+                Responsibility = job.Responsibility,
+                Location = job.Location,
+                Type= job.Type,
+                Qualifications= job.Qualifications,
+                SalaryType = job.SalaryType,
+                StartBudget= job.StartBudget,
+                EndBudget= job.EndBudget,
+                JobExperience = job.JobExperience,
+                JobShift= job.JobShift,
+                JobStatus= job.JobStatus,
+                DeadLine= job.DeadLine,
+                JobPosted= job.JobPosted,
+                Vacancy= job.Vacancy,
+                EmployerId= job.EmployerId,
+                Employer= job.Employer,
+            };*/
             var newJob = await _jobRepository.Add(job);
 
-            return newJob;
+            // Save the product entity to the database using Entity Framework Core or your preferred ORM
+            // dbContext.Products.Add(product);
+            // await dbContext.SaveChangesAsync();
+
+            return Ok(newJob);
 
         }
+        [HttpPost("uploadImage")]
+        public IActionResult UploadImage(IFormFile image)
+        {
+            if (image == null || image.Length == 0)
+                return BadRequest("No image uploaded.");
 
+            // Generate a unique image file name
+            var imageFileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+
+            if (image != null && image.Length > 0)
+            {
+                var filePath = Path.Combine("D:\\JobPortal\\src\\assets\\Images\\", imageFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(fileStream);
+                }
+                var img = imageFileName.ToJson();
+                return Ok(img);
+            }
+
+            return BadRequest("No image file received.");
+        }
+
+
+        [HttpPost("image")]
+        public async Task<ActionResult<Job>> ImageCreate(IFormFile job)
+        {
+            var file = job;
+            var featureJobs = await _jobRepository.GetFeatureJobs();
+            if (!featureJobs.Any())
+            {
+                return BadRequest();
+            }
+            return Ok(featureJobs);
+
+        }
         // PUT api/Job/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
---
+++ JobPortal/Models/Dto/JobDto.cs
@@ -0,0 +1,27 @@
+﻿using JobPortal.Enums;
+
+namespace JobPortal.Models.Dto
+{
+    public class JobDto
+    {
+        public IFormFile Icon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Responsibility { get; set; }
+        public Location Location { get; set; }
+        public JobType Type { get; set; }
+        public Qualification Qualifications { get; set; }
+        public SalaryType SalaryType { get; set; }
+        public double StartBudget { get; set; }
+        public double EndBudget { get; set; }
+        public JobExperience JobExperience { get; set; }
+        public JobShift JobShift { get; set; }
+        public JobStatus JobStatus { get; set; }
+        public DateTime DeadLine { get; set; }
+        public DateTime JobPosted { get; set; } = DateTime.Now;
+        public int Vacancy { get; set; }
+        public Guid EmployerId { get; set; }
+        public Employer? Employer { get; set; }
+
+    }
+}
